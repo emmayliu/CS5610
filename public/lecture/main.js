da76854cd2275b16836ae42f97d9647e82d28de3
emma <liuyujiao@ccs.neu.edu>
2016-02-04 19:55:17 -0800

    work on form

---
+++ public/lecture/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by emma on 2/4/16.
+ */
+(function () {
+    $(init);
+
+    fuction init()
+    {
+        console.log("hello, world");
+
+        $("#clickBtn")
+            .click()
+    }
+})
