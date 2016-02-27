e07c94ea502851452ab469988b04c9f260192ac9
emma <liuyujiao@ccs.neu.edu>
2016-02-27 12:34:49 -0800

    AngularJS practice

---
+++ public/lecture/angularjs/movieAdmin/app.js
@@ -0,0 +1,64 @@
+/**
+ * Created by emma on 2/26/16.
+ */
+(function(){
+    angular
+        .module("MovieAdminApp", [])
+        .controller("MovieController", MovieController);
+
+    function MovieController($scope) {
+        $scope.movies = [
+            {id: 123, title: "Harry", director: "JJ Abrams"},
+            {id: 234, title: "Blade Runner", director: "xx dakg"},
+            {id: 345, title: "Aliens", director: "James"}
+        ];
+
+        //event handler declaration
+        $scope.addMovie = addMovie;
+        $scope.deleteMovie = deleteMovie;
+        $scope.selectMovie = selectMovie;
+        $scope.updateMovie = updateMovie;
+
+
+        // event handler implementation
+        function addMovie(movie) {
+            //console.log("Add movie;" + $scope.movie.title);
+            var newMovie = {
+                id: movie.id,
+                title: movie.title,
+                director: movie.director
+            }
+            $scope.movies.push(newMovie);
+
+            $scope.movie = {};
+        }
+
+        function deleteMovie(movie) {
+            var index = $scope.movies.indexOf(movie);
+            $scope.movies.splice(index, 1);
+        }
+
+        var selectedMovieIndex = null;
+        function selectMovie(movie) {
+            console.log(movie);
+            selectedMovieIndex = $scope.movies.indexOf(movie);
+
+            $scope.movie = {
+                id: movie.id,
+                title: movie.title,
+                director: movie.director
+            }
+        }
+
+        function updateMovie(movie){
+            $scope.movies[selectedMovieIndex] = {
+                id: movie.id,
+                title: movie.title,
+                director: movie.director
+
+            };
+
+
+        }
+    }
+})();
---
+++ public/lecture/js/src.js
@@ -0,0 +1,17 @@
+/**
+ * Created by emma on 2/24/16.
+ */
+
+//alert("Hello World!");
+(function() {
+    var a = 2;
+    var b = 4;
+    var c = Math.pow(a, b);
+    alert(c);
+    var fact = 1;
+    for (var i = 1; i <= 5; i++) {
+        fact = fact * i;
+    }
+})();
+
+
